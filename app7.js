f61db839e3f249093d8e9f5290c79297fa154b24
MirzaAyon <ayonmirza98@gmail.com>
2022-07-02 18:46:46 +0600

    cards,cards image and cards code are showing

---
+++ app7.js
@@ -0,0 +1,54 @@
+// console.log('Connected'); //for testing
+
+// 1. button event handler setup 
+// 2.get input value 
+//3.error handling for string value
+
+const searchButton = () => {
+    // console.log('button setup');
+    const input = document.getElementById('input-value');
+    const error = document.getElementById('error');
+    // console.log(error); 
+    const inputValue = parseInt(input.value);
+    // console.log(inputValue);
+    if (isNaN(inputValue) || inputValue == "") //isNaN check number or string or others
+    {
+        // alert('please enter a number');
+        error.innerText = "please give a number";
+        input.value = "";
+    }
+    else if (inputValue <= 0) {
+        error.innerText = 'please give a positive number';
+        input.value = "";
+    }
+    else {
+        fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`) //dynamic korar jonno templete string anlam and es6 use korlam 
+            //ekhn ami ja value dibo tai ashbe
+            //finally API take dynamic korlam
+            .then(res => res.json())
+
+            .then(data => cardsDisplay(data))
+        //ekhane array take dhorlam
+
+
+        input.value = "";
+    }
+}
+const cardsDisplay = (cards) => {
+
+    cards = cards.cards;
+    console.log(cards);
+    //ekhn array take dhorte partesi
+
+    for (const card of cards) {
+        console.log(card);
+        console.log(card.image);
+        console.log(card.code);
+    }
+
+
+    //object ke for of loop dewa jabe na tai age array take dhorte hbe 
+}
+// for of loop chaliachi
+//now all objects are showing
+
